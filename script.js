450216b9c7dcd10b1990d62bd43db4183ebc5922
eliseschofield <eliseschofield@students.berkeley.net>
2018-11-04 21:38:40 -0800

    Code finished, few test codes

--- script.js
+++ script.js
@@ -29,6 +29,121 @@ function monkey_trouble(a_smile, b_smile)
     return (true)
 }
 
+function string_times(word, num){
+    var newString = '';
+    for(var i = 0; i < num; i++){
+
+        newString = newString +  word;
+
+    }
+    return newString;
+}
+
+function front_times(word, num){
+
+    var newString = '';
+
+    for(var i = 0; i < num; i++){
+
+        newString = newString + word.substr(0, 3);
+    }
+    return newString;
+}
+
+function string_bits(word){
+    var new_string = '';
+
+    for(var i = 0; i <word.length / 2; i++){
+        new_string = new_string + word.substr(i*2 , 1)
+    }
+
+    return new_string;
+}
+
+function caughtSpeeding(speed, birthday){
+    if(birthday == true){
+        speed = speed -5;
+    }
+
+    if(speed <= 60){
+        return 0;
+    }
+    if(speed <= 80 && speed >= 61){
+        return 1;
+    }
+
+    return 2;
+
+}
+
+function fizz_buzz(num){
+    if(num%3 == 0 && num%5 == 0){
+        return 'FizzBuzz';
+    }
+
+    if(num%3 == 0){
+        return 'Fizz';
+    }
+    if(num%5 ==0){
+        return 'Buzz';
+    }
+    return num + '!';
+}
+
+function teaParty(tea, candy){
+    if(tea < 5 || candy < 5){
+        return 0;
+    }
+    if(tea >= candy*2 || candy >= tea*2){
+        return 2;
+    }
+    if(tea >=5 && candy >= 5){
+        return 1;
+    }
+
+
+}
+
+
+function blackjack(x, y){
+    var diffX = 21 - x
+    var diffY = 21-y
+
+    if(x >21 && y>21){
+        return 0;
+    }
+    if(x >21){
+        return y;
+    }
+    if(y>21){
+        return x;
+    }
+
+    if(diffX>diffY){
+        return y;
+    }
+    if(diffX<diffY){
+        return x;
+    }
+}
+
+function loneSum(x, y, z){
+    if(x==y && y==z && x==z){
+        return 0;
+    }
+    if(x == y){
+        return z;
+    }
+    if(x== z){
+        return y;
+    }
+    if(y==z){
+        return x;
+    }
+    return x + y + z;
+}
+
+
 
 //write second method
 function nextOne(param1, param2) {
